b6e8cfd581e468761b3af6b5755d7380d0095498
frheault <francois.m.rheault@usherbrooke.ca>
2021-04-12 14:06:49 -0500

    Update for ensemble

--- code/launch_rbx_flow_wrapper.sh
+++ code/launch_rbx_flow_wrapper.sh
@@ -6,17 +6,16 @@ OUT_DIR=${2}
 N_SUBJ=${3}
 N_SESS=${4}
 IN_FA=${5}
-IN_TRACKING_LOCAL=${6}
-IN_TRACKING_PFT=${7}
+IN_TRACKING_ENSEMBLE=${6}
 
 cd /TMP/
 mkdir raw/${N_SUBJ}_${N_SESS} -p
 cp ${IN_DIR}/${IN_FA} raw/${N_SUBJ}_${N_SESS}/fa.nii.gz
-scil_streamlines_math.py concatenate ${IN_DIR}/${IN_TRACKING_LOCAL} ${IN_DIR}/${IN_TRACKING_PFT} raw/${N_SUBJ}_${N_SESS}/tracking.trk
+cp ${IN_DIR}/${IN_TRACKING_ENSEMBLE} raw/${N_SUBJ}_${N_SESS}/tracking.trk
 
 # Relevant parameters for Tractoflow
-NB_RUN=${8}
-VOTE_RATIO=${9}
+NB_RUN=${7}
+VOTE_RATIO=${8}
 
 /nextflow /rbx_flow/main.nf \
 	--input raw/ --atlas_config /rbx_pop_average/config.json --atlas_anat /rbx_pop_average/mni_masked.nii.gz \
@@ -37,6 +36,6 @@ xvfb-run -a --server-num=$((65536+$$)) --server-args="-screen 0 1600x1280x24 -ac
 
 python3.7 /CODE/generate_rbx_flow_spider_pdf.py ${N_SUBJ}_${N_SESS} 
 
-cp report.pdf report.html results_rbx/*/readme.txt ${OUT_DIR}/
-cp -L results_rbx/*/Clean_Bundles/ ${OUT_DIR}/bundles
-cp -L results_rbx/*/Transform_Centroids/ ${OUT_DIR}/centroids
+cp report.pdf report.html ${OUT_DIR}/
+cp -rL results_rbx/*/Clean_Bundles/ ${OUT_DIR}/bundles
+cp -rL results_rbx/*/Transform_Centroids/ ${OUT_DIR}/centroids
